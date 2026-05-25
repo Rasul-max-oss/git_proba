ffa14425e812f101e6a82ce23e3c0ac387eefb88
Rasul <rasul.rzaev.2014@mail.ru>
2026-05-25 22:24:40 +0800

    добавил в игру цикл

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random 
-n = random.randint(0,1)
-if n == 0: print("Орёл")
-else: print("Решка")
+while True:
+    n = random.randint(0,1)
+    if n == 0: print("Орёл")
+    else: print("Решка")
