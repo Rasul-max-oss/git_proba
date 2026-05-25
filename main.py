75ac67134b29daf6776f6d037b8165bc3263d856
Rasul <rasul.rzaev.2014@mail.ru>
2026-05-25 22:33:37 +0800

    добавил в игру ввод и выход

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random 
 while True:
+    e = input("Нажмите Enter или выход")
+    if e == "выход":
+        break
     n = random.randint(0,1)
     if n == 0: print("Орёл")
     else: print("Решка")
